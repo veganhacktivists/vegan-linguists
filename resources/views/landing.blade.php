f2db9c6c515eac42101679c3978ac3bb233c8da8
Gerard O'Neill <grardb@gmail.com>
2021-09-21 21:15:14 +0200

    Add Patreon banner to landing page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -46,7 +46,7 @@
             <div class="absolute inset-x-0 bottom-0 h-1/2"></div>
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="relative px-4 py-16 sm:px-6 lg:px-8">
-                    <x-icon-logo-with-text class="h-40 sm:h-64 mx-auto" />
+                    <x-icon-logo-with-text class="h-40 sm:h-64 mx-auto max-w-full" />
                     <h1 class="text-center font-extrabold tracking-tight text-3xl mt-5">
                         {{ __('Have vegan content? Get it translated.') }}
                     </h1>
@@ -156,7 +156,9 @@
                         {{ __('Who are the Vegan Linguists?') }}
                     </p>
                     <p class="mt-5 text-xl text-brandBeige-300">
-                        {{ __("Vegan Linguists is a project created by the Vegan Hacktivists.  Since the beginning of our journey, we've had hundreds of vegans approach us with the desire to help, but without the technical skills necessary to build software. However, these activists did have something equally as valuable: the ability to speak multiple languages.") }}
+                        {!! __("Vegan Linguists is a project created by the :veganHacktivists.  Since the beginning of our journey, we've had hundreds of vegans approach us with the desire to help, but without the technical skills necessary to build software. However, these activists did have something equally as valuable: the ability to speak multiple languages.", [
+    'veganHacktivists' => '<a href="https://veganhacktivists.org" target="_blank" class="text-brandGreen-400 hover:text-brandGreen-500">Vegan Hacktivists</a>',
+]) !!}
                     </p>
                     <p class="mt-5 text-xl text-brandBeige-300">
                         {{ __("We've realized that there are huge accessibility gaps when it comes to information related to veganism, simply due to language barriers. However, there are also many people out there who want to help, but haven't been provided the tools or the opportunity to do so. Knowing these things, it only made sense to build a service that would help multiply the effectiveness of existing work, while also enabling an entirely new group of activists.") }}
@@ -181,6 +183,29 @@
                         {{ __('Become a translator') }}
                     </a>
                 </div>
+            </div>
+        </div>
+
+        <div class="bg-brandGreen-400">
+            <div
+                 class="max-w-7xl w-5/6 md:w-auto mx-auto text-white flex flex-col md:flex-row justify-between md:items-center py-8 md:px-6 lg:px-8 gap-6 text-center md:text-left">
+                <div class="flex gap-6">
+                    <x-icon-patreon class="h-16 w-16 hidden md:inline-block" />
+                    <div class="flex flex-col gap-2">
+                        <h4 class="text-2xl">{{ __('Please consider supporting us on Patreon!') }}</h4>
+                        <p class="text-xl">
+                            {{ __('This free-to-use service would not be possible without your support. Thank you!') }}
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    <a href="https://patreon.com/veganhacktivists"
+                       target="_blank"
+                       class=" bg-white hover:bg-brandGreen-50 text-brandGreen-400 font-bold rounded-md px-8 py-2 text-lg text-center whitespace-nowrap">
+                        {{ __('Support us') }}
+                    </a>
+                </div>
+
             </div>
         </div>
     </main>
@@ -366,28 +391,14 @@
                        class="text-gray-400 hover:text-gray-500"
                        target="_blank">
                         <span class="sr-only">Instagram</span>
-                        <svg class="h-6 w-6"
-                             fill="currentColor"
-                             viewBox="0 0 24 24"
-                             aria-hidden="true">
-                            <path fill-rule="evenodd"
-                                  d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-                                  clip-rule="evenodd" />
-                        </svg>
+                        <x-icon-instagram class="h-6 w-6" />
                     </a>
 
                     <a href="https://github.com/veganhacktivists"
                        class="text-gray-400 hover:text-gray-500"
                        target="_blank">
                         <span class="sr-only">GitHub</span>
-                        <svg class="h-6 w-6"
-                             fill="currentColor"
-                             viewBox="0 0 24 24"
-                             aria-hidden="true">
-                            <path fill-rule="evenodd"
-                                  d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                                  clip-rule="evenodd" />
-                        </svg>
+                        <x-icon-github class="h-6 w-6" />
                     </a>
                 </div>
                 <p class="mt-8 text-base text-gray-500 md:mt-0 md:order-1">
