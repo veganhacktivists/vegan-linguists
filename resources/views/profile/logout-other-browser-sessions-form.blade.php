57cacf3d3015ead6299eda66d53170eb3a4c210d
Gerard O'Neill <grardb@gmail.com>
2021-09-21 20:02:19 +0200

    Bring landing illustrations closer to window edge

--- resources/views/profile/logout-other-browser-sessions-form.blade.php
+++ resources/views/profile/logout-other-browser-sessions-form.blade.php
@@ -14,17 +14,39 @@
 
         @if (count($this->sessions) > 0)
             <div class="mt-5 space-y-6">
-                <!-- Other Browser Sessions -->
+                {{-- Other Browser Sessions --}}
                 @foreach ($this->sessions as $session)
                     <div class="flex items-center">
                         <div>
                             @if ($session->agent->isDesktop())
-                                <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="w-8 h-8 text-gray-500">
-                                    <path d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
+                                <svg fill="none"
+                                     stroke-linecap="round"
+                                     stroke-linejoin="round"
+                                     stroke-width="2"
+                                     viewBox="0 0 24 24"
+                                     stroke="currentColor"
+                                     class="w-8 h-8 text-gray-500">
+                                    <path
+                                          d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z">
+                                    </path>
                                 </svg>
                             @else
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" class="w-8 h-8 text-gray-500">
-                                    <path d="M0 0h24v24H0z" stroke="none"></path><rect x="7" y="4" width="10" height="16" rx="1"></rect><path d="M11 5h2M12 17v.01"></path>
+                                <svg xmlns="http://www.w3.org/2000/svg"
+                                     viewBox="0 0 24 24"
+                                     stroke-width="2"
+                                     stroke="currentColor"
+                                     fill="none"
+                                     stroke-linecap="round"
+                                     stroke-linejoin="round"
+                                     class="w-8 h-8 text-gray-500">
+                                    <path d="M0 0h24v24H0z"
+                                          stroke="none"></path>
+                                    <rect x="7"
+                                          y="4"
+                                          width="10"
+                                          height="16"
+                                          rx="1"></rect>
+                                    <path d="M11 5h2M12 17v.01"></path>
                                 </svg>
                             @endif
                         </div>
@@ -52,16 +74,19 @@
         @endif
 
         <div class="flex items-center mt-5">
-            <x-jet-button type="submit" wire:click="confirmLogout" wire:loading.attr="disabled">
+            <x-jet-button type="submit"
+                          wire:click="confirmLogout"
+                          wire:loading.attr="disabled">
                 {{ __('Log Out Other Browser Sessions') }}
             </x-jet-button>
 
-            <x-jet-action-message class="ml-3" on="loggedOut">
+            <x-jet-action-message class="ml-3"
+                                  on="loggedOut">
                 {{ __('Done.') }}
             </x-jet-action-message>
         </div>
 
-        <!-- Log Out Other Devices Confirmation Modal -->
+        {{-- Log Out Other Devices Confirmation Modal --}}
         <x-jet-dialog-modal wire:model="confirmingLogout">
             <x-slot name="title">
                 {{ __('Log Out Other Browser Sessions') }}
@@ -70,28 +95,31 @@
             <x-slot name="content">
                 {{ __('Please enter your password to confirm you would like to log out of your other browser sessions across all of your devices.') }}
 
-                <div class="mt-4" x-data="{}" x-on:confirming-logout-other-browser-sessions.window="setTimeout(() => $el.querySelector('input[type=password]').focus(), 250)">
+                <div class="mt-4"
+                     x-data="{}"
+                     x-on:confirming-logout-other-browser-sessions.window="setTimeout(() => $el.querySelector('input[type=password]').focus(), 250)">
                     <x-password-input class="mt-1 block"
-                                containerClass="w-3/4"
-                                placeholder="{{ __('Password') }}"
-                                wire:model.defer="password"
-                                wire:keydown.enter="logoutOtherBrowserSessions" />
+                                      containerClass="w-3/4"
+                                      placeholder="{{ __('Password') }}"
+                                      wire:model.defer="password"
+                                      wire:keydown.enter="logoutOtherBrowserSessions" />
 
-                    <x-jet-input-error for="password" class="mt-2" />
+                    <x-jet-input-error for="password"
+                                       class="mt-2" />
                 </div>
             </x-slot>
 
             <x-slot name="footer">
-                <x-jet-secondary-button type="button" wire:click="$toggle('confirmingLogout')" wire:loading.attr="disabled">
+                <x-jet-secondary-button type="button"
+                                        wire:click="$toggle('confirmingLogout')"
+                                        wire:loading.attr="disabled">
                     {{ __('Cancel') }}
                 </x-jet-secondary-button>
 
-                <x-jet-button
-                    class="ml-2"
-                    type="submit"
-                    wire:click="logoutOtherBrowserSessions"
-                    wire:loading.attr="disabled"
-                >
+                <x-jet-button class="ml-2"
+                              type="submit"
+                              wire:click="logoutOtherBrowserSessions"
+                              wire:loading.attr="disabled">
                     {{ __('Log Out Other Browser Sessions') }}
                 </x-jet-button>
             </x-slot>
