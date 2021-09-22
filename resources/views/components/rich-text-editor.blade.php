4842fb2fc1fdbb058f81d88e9687a5f3f32b7b67
Gerard O'Neill <grardb@gmail.com>
2021-09-22 13:29:17 +0200

    Fix double rich text editor bug

--- resources/views/components/rich-text-editor.blade.php
+++ resources/views/components/rich-text-editor.blade.php
@@ -1,9 +1,16 @@
+@push('scripts')
+    {{-- This prevents the editor breaking due to Livewire/Turbolinks issues --}}
+    <meta name="turbolinks-visit-control"
+          content="reload">
+@endpush
+
 @props(['content' => '', 'isReadOnly' => false, 'disableStyles' => false, 'autoFocus' => false])
 
 <div x-data="richTextEditor({{ $isReadOnly ? 'true' : 'false' }}, {{ $autoFocus ? ' true' : 'false' }})"
-    {{ $attributes->merge([
+     {{ $attributes->merge([
     'class' => $disableStyles ? 'styles-disabled' : '',
 ]) }}>
-    <div x-ref="editorContent" class="hidden">{!! $content !!}</div>
+    <div x-ref="editorContent"
+         class="hidden">{!! $content !!}</div>
     <div x-ref="editorContainer"></div>
 </div>
