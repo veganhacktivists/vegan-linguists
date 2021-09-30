3d877bda3a8d1808b49304a52a7c01d358e8e434
Gerard O'Neill <grardb@gmail.com>
2021-09-30 17:01:03 +0200

    Add brand elements to email verification page

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,21 +1,22 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <x-icon-logo-with-text class="w-full" />
         </x-slot>
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+        <div class="mb-4 text-sm text-brandBrown-600">
+            {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
         </div>
 
         @if (session('status') == 'verification-link-sent')
-            <div class="mb-4 font-medium text-sm text-green-600">
+            <div class="mb-4 font-medium text-sm text-brandGreen-500">
                 {{ __('A new verification link has been sent to the email address you provided during registration.') }}
             </div>
         @endif
 
         <div class="mt-4 flex items-center justify-between">
-            <form method="POST" action="{{ route('verification.send') }}">
+            <form method="POST"
+                  action="{{ route('verification.send') }}">
                 @csrf
 
                 <div>
@@ -25,10 +26,12 @@
                 </div>
             </form>
 
-            <form method="POST" action="{{ route('logout') }}">
+            <form method="POST"
+                  action="{{ route('logout') }}">
                 @csrf
 
-                <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900">
+                <button type="submit"
+                        class="underline text-sm text-brandBrown-600 hover:text-brandBrown-900">
                     {{ __('Log Out') }}
                 </button>
             </form>
