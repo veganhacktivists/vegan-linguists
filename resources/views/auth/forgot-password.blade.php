216ef74cff88a69378ec4ca86e9380cd3d1a5b26
Gerard O'Neill <grardb@gmail.com>
2021-09-30 17:01:55 +0200

    Add brand elements to 'forgot password' page

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,27 +1,35 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <x-icon-logo-with-text class="w-full" />
         </x-slot>
 
-        <div class="mb-4 text-sm text-gray-600">
+        <div class="mb-4 text-sm text-brandBrown-600">
             {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
         </div>
 
         @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
+            <div class="mb-4 font-medium text-sm text-brandGreen-500">
                 {{ session('status') }}
             </div>
         @endif
 
         <x-jet-validation-errors class="mb-4" />
 
-        <form method="POST" action="{{ route('password.email') }}">
+        <form method="POST"
+              action="{{ route('password.email') }}">
             @csrf
 
             <div class="block">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-jet-label for="email"
+                             value="{{ __('Email') }}" />
+                <x-jet-input id="email"
+                             class="block mt-1 w-full"
+                             type="email"
+                             name="email"
+                             :value="old('email')"
+                             required
+                             autofocus />
             </div>
 
             <div class="flex items-center justify-end mt-4">
