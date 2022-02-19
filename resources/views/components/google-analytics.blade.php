f10e1a54517674c03bc3b69c7c7f708ee4f10c04
Gerard O'Neill <grardb@gmail.com>
2022-02-19 23:29:25 +0100

    Attempt to fix Google Analytics reporting

--- resources/views/components/google-analytics.blade.php
+++ resources/views/components/google-analytics.blade.php
@@ -2,9 +2,13 @@
     <script async
             src="https://www.googletagmanager.com/gtag/js?id=G-Y7TQD4J5Z7"></script>
     <script>
-        window.dataLayer = window.dataLayer || [];
+        (function() {
+            window.dataLayer = window.dataLayer || [];
 
-        dataLayer.push(['js', new Date()]);
-        dataLayer.push(['config', 'G-Y7TQD4J5Z7']);
+            function gtag(){ dataLayer.push(arguments) }
+
+            gtag('js', new Date());
+            gtag('config', 'G-Y7TQD4J5Z7');
+        })()
     </script>
 @endif
