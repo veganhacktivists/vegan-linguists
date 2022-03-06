a5baef92dab2498daf2569296947d02e09405964
Gerard O'Neill <grardb@gmail.com>
2022-03-06 18:48:42 +0100

    Properly size the logo on Firefox

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -1,15 +1,8 @@
 <x-guest-layout>
     <div class="py-4">
-        <div
-             class="
-         min-h-screen
-         flex
-         flex-col
-         items-center
-         pt-6
-         sm:pt-0">
+        <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0">
             <div>
-                <x-icon-logo-with-text class="w-full" />
+                <x-icon-logo-with-text class="w-56 sm:w-80" />
             </div>
 
             <div class="w-full sm:max-w-2xl mt-6 p-6 bg-white shadow-md overflow-hidden sm:rounded-lg prose">
