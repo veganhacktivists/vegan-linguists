cb844b0e1ec05ba5a99142bfb1bd3bd7b300048a
Gerard O'Neill <grardb@gmail.com>
2022-07-30 11:04:29 +0200

    Add missing period in the cookie banner

--- resources/views/components/cookie-banner.blade.php
+++ resources/views/components/cookie-banner.blade.php
@@ -1,7 +1,7 @@
 <div class="fixed bottom-0 inset-x-0 shadow-lg p-3 bg-brand-beige-100 border border-t-brand-beige-500 flex justify-between text-brand-brown-800 text-sm" id="cookie-banner" x-cloak>
     <p class="flex items-center pr-6">
         <span>
-            {!! __('We use cookies to help us improve Vegan Linguists. To learn more, view our :privacyPolicyLink, or opt out by installing :browserAddonLink', [
+            {!! __('We use cookies to help us improve Vegan Linguists. To learn more, view our :privacyPolicyLink, or opt out by installing :browserAddonLink.', [
                 'privacyPolicyLink' => '<a href="' . route('policy.show') .'" class="text-brand-clay-500 hover:text-brand-clay-600">' . __('Privacy Policy') . '</a>',
                 'browserAddonLink' => '<a href="https://tools.google.com/dlpage/gaoptout" class="text-brand-clay-500 hover:text-brand-clay-600" target="_blank">Google Analytics Opt-out Browser Add-on</a>',
             ]) !!}
