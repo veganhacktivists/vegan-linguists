caa412d04c65bd9acb81260e0d6df113eb2707c3
Joaquín Triñanes <joaquin@veganhacktivists.org>
2024-01-30 19:13:00 +0100

    Remove non-compatible laravel-page-speed

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,10 +22,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-
-        LaravelPageSpeed\Middleware\ElideAttributes::class,
-        LaravelPageSpeed\Middleware\InsertDNSPrefetch::class,
-        LaravelPageSpeed\Middleware\RemoveComments::class,
     ];
 
     /**
