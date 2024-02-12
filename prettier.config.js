72af7a864e745eaf9571a9f2650feaf332a944dd
Joaquín Triñanes <joaquin@veganhacktivists.org>
2024-02-12 14:43:27 +0100

    Fix prettier blade formatting

--- prettier.config.js
+++ prettier.config.js
@@ -1,48 +1,49 @@
-{
-  "overrides": [
+export default {
+  plugins: ['@shufo/prettier-plugin-blade'],
+  overrides: [
     {
-      "files": "*.js",
-      "options": {
-        "arrowParens": "always",
-        "parser": "babel",
-        "printWidth": 80,
-        "semi": false,
-        "singleQuote": true,
-        "tabWidth": 2,
-        "trailingComma": "all"
-      }
+      files: '*.js',
+      options: {
+        arrowParens: 'always',
+        parser: 'babel',
+        printWidth: 80,
+        semi: false,
+        singleQuote: true,
+        tabWidth: 2,
+        trailingComma: 'all',
+      },
     },
     {
-      "files": "*.css",
-      "options": {
-        "parser": "css",
-        "printWidth": 80,
-        "singleQuote": true,
-        "tabWidth": 2
-      }
+      files: '*.css',
+      options: {
+        parser: 'css',
+        printWidth: 80,
+        singleQuote: true,
+        tabWidth: 2,
+      },
     },
     {
-      "files": "*.json",
-      "options": {
-        "parser": "json",
-        "printWidth": 80,
-        "tabWidth": 2
-      }
+      files: '*.json',
+      options: {
+        parser: 'json',
+        printWidth: 80,
+        tabWidth: 2,
+      },
     },
     {
-      "files": "*.php",
-      "options": {
-        "parser": "php",
-        "singleQuote": true
-      }
+      files: '*.php',
+      options: {
+        parser: 'php',
+        singleQuote: true,
+      },
     },
     {
-      "files": "*.blade.php",
-      "options": {
-        "parser": "blade",
-        "sortTailwindcssClasses": true,
-        "tabWidth": 2
-      }
-    }
-  ]
+      files: '*.blade.php',
+      options: {
+        parser: 'blade',
+        sortTailwindcssClasses: true,
+        tabWidth: 2,
+      },
+    },
+  ],
 }
